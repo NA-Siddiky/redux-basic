f6fed51978e30c794872f885ced09f79c041dc8d
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-23 16:54:38 +0600

    project stater

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <>
+      <div className="main-div">
+
+
+        <div class="container">
+
+          <h1>Increment/Decrement counter</h1>
+          <h4>using React and Redux</h4>
+
+          <div class="quantity">
+            <a class="quantity__minus"><span>-</span></a>
+            <input name="quantity" type="text" class="quantity__input" />
+            <a class="quantity__plus"><span>+</span></a>
+          </div>
+
+        </div>
+      </div>
+    </>
   );
 }
 
