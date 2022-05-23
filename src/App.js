f68c453f97c9cfe5d2938034011932f9ce56ebde
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-23 17:33:10 +0600

    redux added

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ function App() {
       <div className="main-div">
 
 
-        <div class="container">
+        <div className="container">
 
           <h1>Increment/Decrement counter</h1>
           <h4>using React and Redux</h4>
 
-          <div class="quantity">
-            <a class="quantity__minus"><span>-</span></a>
-            <input name="quantity" type="text" class="quantity__input" value="0" />
-            <a class="quantity__plus"><span>+</span></a>
+          <div className="quantity">
+            <a className="quantity__minus"><span>-</span></a>
+            <input name="quantity" type="text" className="quantity__input" value="0" />
+            <a className="quantity__plus"><span>+</span></a>
           </div>
 
         </div>
